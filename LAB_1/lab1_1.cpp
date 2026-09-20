a76078fb3f14797b3d9901b47fae8d413bc1be6d
sncsoew <egorich-06@mail.ru>
2026-09-20 13:06:37 +0500

    Добавлена функция printArray для вывода массива на экран

--- LAB_1/lab1_1.cpp
+++ LAB_1/lab1_1.cpp
@@ -6,8 +6,15 @@ void fillArray(int (&arr)[10]){
     }
 }
 
+void printArray(int (&arr)[10]){
+    for(auto n: arr){
+        std::cout << n << " ";
+    }
+}
+
 int main(){
     int array[10];
     fillArray(array);
+    printArray(array);
     return 0;
 }
