78584f90ff1e66d5ad771c53338d4167c8845229
sncsoew <egorich-06@mail.ru>
2026-09-20 14:14:15 +0500

    Добавлена функция swapElements для перестановки элементов массива по их индексам. Также добавлена русская локаль

--- LAB_1/lab1_1.cpp
+++ LAB_1/lab1_1.cpp
@@ -12,9 +12,24 @@ void printArray(int (&arr)[10]){
     }
 }
 
+void swapElements(int &a,int &b,int (&arr)[10]){
+    int t = arr[a-1];
+    arr[a-1] = arr[b-1];
+    arr[b-1] = t;
+}
+
 int main(){
+    setlocale(LC_ALL, ".UTF-8");
     int array[10];
+    std::cout << "Введите 10 чисел:" << std::endl;
     fillArray(array);
     printArray(array);
+    int one;
+    int two;
+    std::cout << "\nВведите первый индекс:" << std::endl;
+    std::cin >> one;
+    std::cout << "Введите второй индекс:" << std::endl;
+    std::cin >> two;
+    swapElements(one, two, array);
     return 0;
 }
