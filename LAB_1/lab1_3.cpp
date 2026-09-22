c6ad2ca47cc3ce23ec214f065fbda55686ad5b49
sncsoew <egorich-06@mail.ru>
2026-09-22 16:13:47 +0500

    Добавлена функция reSizeArray,которая изменяет размер массива с N элементов на M элементов.

--- LAB_1/lab1_3.cpp
+++ LAB_1/lab1_3.cpp
@@ -7,7 +7,7 @@ struct SafeArray {
 
 SafeArray createArray(int size){
     SafeArray arr;
-    arr.data = new int[size];
+    arr.data = new int[size]();
     arr.size = size;
     return arr;
 }
@@ -25,11 +25,40 @@ void printSafe(const SafeArray& arr){
     for(int i = 0; i <arr.size; ++i){
         std::cout << arr.data[i] << " ";
     }
+    std::cout << std::endl;
+}
+
+void reSizeArray(SafeArray& arr, int M){
+    int elementsToCopy;
+    if (M < arr.size){
+        for(int i = M; i <arr.size; i++){
+            std::cout << arr.data[i] <<" ";
+        }
+    std::cout << std::endl;
+    elementsToCopy = M;
+    } else{
+        elementsToCopy = arr.size;
+    }
+    int* newarr = new int[M];
+    for(int i = 0; i < elementsToCopy; i++){
+        newarr[i] = arr.data[i];
+    }
+    for(int i =arr.size; i < M; i++){
+        newarr[i] = 0;
+    }
+    delete[] arr.data;
+    arr.data = newarr;
+    arr.size = M;
 }
 
 int main(){
     setlocale(LC_ALL, ".UTF-8");
     SafeArray arr = createArray(5);
-    getElement(arr,3) = 100;
+    getElement(arr,1) = 100;
     printSafe(arr);
+    reSizeArray(arr,3);
+    delete[] arr.data;
+    arr.data = nullptr;
+    arr.size = 0;
+    return 0;
 }
