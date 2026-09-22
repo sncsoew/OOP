cb5f0f1ab6ee688b8497dd8fefb08f1a1f389587
sncsoew <egorich-06@mail.ru>
2026-09-22 14:08:02 +0500

    Добавлена функция getElements, дающая доступ к элементам по индексу и проверяющая коректность вводимого индекса

--- LAB_1/lab1_3.cpp
+++ LAB_1/lab1_3.cpp
@@ -12,6 +12,17 @@ SafeArray createArray(int size){
     return arr;
 }
 
+int& getElement(SafeArray& arr, int index){
+    static int wroongarr = 0;
+    if (index < 0 || index >= arr.size){
+        std::cout << "Ошибка! превышен размер массива" << std::endl;
+        return wroongarr;
+    }
+    return arr.data[index];
+}
+
 int main(){
-    
+    setlocale(LC_ALL, ".UTF-8");
+    SafeArray arr = createArray(5);
+    getElement(arr,6) = 100;
 }
